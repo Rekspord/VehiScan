0a03bd0dfc3ad1c38a4aab4d44ca3d5924d0cf6c
Rekspord <main.rexford.llanes@cvsu.edu.ph>
2024-12-30 18:09:44 +0800

    Main Screen UI

--- app/(app)/main.js
+++ app/(app)/main.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import { auth } from '../../firebaseConfig'; // Adjust the import path as needed
 import { signOut } from 'firebase/auth';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import Entypo from '@expo/vector-icons/Entypo';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Main = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Not yet scanned');
 
   useEffect(() => {
     (async () => {
@@ -31,24 +33,48 @@ const Main = () => {
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the Main Page!</Text>
-      {scanned && (
-        <TouchableOpacity onPress={() => setScanned(false)} style={{ marginTop: 20 }}>
-          <Text style={{ color: 'blue' }}>Tap to Scan Again</Text>
+    <View style={{ flex: 1, backgroundColor: 'gray', justifyContent: 'space-between' }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 16 }}>
+        <TouchableOpacity>
+          <Ionicons name="person" size={30} color="black" />
         </TouchableOpacity>
-      )}
-      <TouchableOpacity
-        style={{
-          marginTop: 20,
-          padding: 10,
-          backgroundColor: 'red',
-          borderRadius: 5,
-        }}
-        onPress={handleLogout}
-      >
-        <Text style={{ color: 'white' }}>Log Out</Text>
-      </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={{ backgroundColor: 'gray', width: 40, height: 40, borderRadius: 20, alignItems: 'center', justifyContent: 'center' }}>
+            <Entypo name="menu" size={40} color="black" />
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ backgroundColor: 'darkgray', width: 320, height: 320, borderRadius: 16, alignItems: 'center', justifyContent: 'center' }}>
+          {/* QR Code Scanner */}
+          
+          {scanned && (
+            <TouchableOpacity onPress={() => setScanned(false)} style={{ marginTop: 20 }}>
+              <Text style={{ color: 'blue' }}>Tap to Scan Again</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 16, paddingBottom: 16 }}>
+        <TouchableOpacity>
+          <View style={{ backgroundColor: 'gray', width: 40, height: 40, borderRadius: 20, alignItems: 'center', justifyContent: 'center' }}>
+            <Fontisto name="history" size={30} color="black" />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            marginTop: 20,
+            padding: 10,
+            backgroundColor: 'red',
+            borderRadius: 5,
+          }}
+          onPress={handleLogout}
+        >
+          <Text style={{ color: 'white' }}>Log Out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
