d2e1f79b66df0736286d7df2a7f30be93f4f2f16
Rekspord <main.rexford.llanes@cvsu.edu.ph>
2025-01-12 23:26:56 +0800

    HistoryPage Backend

--- app/(app)/history.js
+++ app/(app)/history.js
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { vehiclesCollection, db, app } from '../../firebaseConfig';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 const History = () => {
   const [expandedVehicle, setExpandedVehicle] = useState(null);
+  const [scannedVehicles, setScannedVehicles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [vehicles, setVehicles] = useState([]); // Initial empty array of users
 
-  const scannedVehicles = [
-    { id: '1', type: 'Car', data: 'Audi A4' },
-    { id: '2', type: 'Truck', data: 'Ford F-150' },
-    { id: '3', type: 'Motorcycle', data: 'Harley Davidson' },
-    // Add more scanned vehicles here
-  ];
+  const firebase = app;
+  const firestore = db;
+
+  useEffect(() => {
+
+    const vehiclesCollectionRef = collection(firestore, 'vehicles');
+  
+
+    const unsubscribe = onSnapshot(vehiclesCollectionRef, (querySnapshot) => {
+      const vehicles = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        vehicles.push({id: doc.id, licensePlate: data.licensePlate, model: data.model, year: data.year});
+      });
+      console.log(vehicles);
+      setVehicles(vehicles); 
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const renderVehicle = ({ item }) => (
     <TouchableOpacity
@@ -31,12 +51,35 @@ const History = () => {
     </TouchableOpacity>
   );
 
+  if (loading) {
+    return (
+      <View className="flex-1 justify-center items-center bg-gray-600 p-4">
+        <ActivityIndicator size="large" color="#fff" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View className="flex-1 justify-center items-center bg-gray-600 p-4">
+        <Text className="text-lg text-red-500">{error}</Text>
+      </View>
+    );
+  }
+
   return (
     <View className="flex-1 justify-center items-center bg-gray-600 p-4">
       <Text className="text-2xl text-white text-center font-bold">HISTORY</Text>
       <FlatList
-        data={scannedVehicles}
-        renderItem={renderVehicle}
+        data={vehicles} 
+        renderItem={({ item }) => (
+          <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Vehicle ID: {item.id}</Text>
+            <Text>License Plate: {item.licensePlate}</Text>
+            <Text>Model: {item.model}</Text>
+            <Text>Year: {item.year}</Text>
+          </View>
+        )}
         keyExtractor={item => item.id}
         className="mt-4 w-full"
       />
